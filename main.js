3bc82277bc8e0fb74a6b7ff815887876ee551d30
Gaël Neumüller <paradox72400@hotmail.com>
2019-04-21 15:13:14 +0200

    added missing comments

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Cree la fenetre du navigateur
   let win = new BrowserWindow({ width: 800, height: 600 })
-
   // and load the index.html of the app.
   win.loadFile('content/index.html')
-
+  // Cache la barre d'outil
   win.setMenuBarVisibility(false)
 }
 
