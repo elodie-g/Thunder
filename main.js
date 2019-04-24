5471fd8d258cd7d6c6de98e7748451d7ea318ce2
Gaël Neumüller <paradox72400@hotmail.com>
2019-04-24 08:15:26 +0200

    essai pour faire un bouton

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ function createWindow () {
   // Cree la fenetre du navigateur
   let win = new BrowserWindow({ width: 800, height: 600, show: true })
   // and load the index.html of the app.
-  win.loadFile('content/index.html')
+  win.loadFile('test.html')
   // Cache la barre d'outil
   win.setMenuBarVisibility(false)
 }
 
 app.on('ready', createWindow)
 
-app.on('blur', (e, cmd) => {
-  console.log('app lost focus!')
-})
+console.log("Window created");
