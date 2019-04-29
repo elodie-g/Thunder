468091a95fb7c69a013ee9aa2a515773324ae9f8
Gaël Neumüller <paradox72400@hotmail.com>
2019-04-29 07:44:48 +0200

    testing things

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const fenetreNavigateur = electron.BrowserWindow;
 
 function creationFenetre () {
   // Cree la fenetre du navigateur
-  let fenetrePrincipale = new fenetreNavigateur({ width: 800, height: 600, show: true , frame: true})
+  let fenetrePrincipale = new fenetreNavigateur({
+    width: 800,
+    height: 600,
+    show: true,
+    frame: true
+  })
   // and load the index.html of the app.
   fenetrePrincipale.loadFile('content/index.html')
   // Cache la barre d'outil
